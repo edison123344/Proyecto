326042146e026d58bc4d388246a7e45ed407829a
edison123344 <45432357+edison123344@users.noreply.github.com>
2022-07-03 23:25:30 -0500

    creacion de la busqueda
    creacion de la busqueda por titulo,nombre,tema

--- repositorio/material/urls.py
+++ repositorio/material/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('material/<int:material_id>/', views.material, name="material"),
     path('temasContenido/<int:temasContenido_id>/', views.temasContenido, name="temasContenido"),
+    #pediente 
     path('contenido/<int:contenido_id>/', views.contenido, name="contenido"),
     
     path('list', views.list, name="list"),
--- repositorio/material/views.py
+++ repositorio/material/views.py
@@ -1,8 +1,11 @@
+from asyncio.windows_events import NULL
 from django.http import Http404
-from django.shortcuts import get_list_or_404, render, HttpResponse
+from django.shortcuts import get_list_or_404, render, HttpResponse,get_object_or_404
 from .models import Material,Tema,TemasContenido,Contenido
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+
 # Create your views here.
+lista =NULL
 def home(request):
    material = Material.objects.all()
    temas = Tema.objects.all
@@ -13,13 +16,19 @@ def material(request,material_id):
 def temasContenido(request,temasContenido_id):
    temasContenido=get_object_or_404(TemasContenido, id=temasContenido_id)
    return render(request,"material/content.html",{'temasContenido':temasContenido})
+   #pendiente
 def contenido(request,contenido_id):
    contenido=get_object_or_404(Contenido, id=contenido_id)
    return render(request,"material/download.html",{'contenido':contenido})
 
 
 def list(request):
-   return render(request,"material/list.html")
+   busqueda =request.GET.get('buscar')
+   if busqueda:
+      temas=Tema.objects.filter(Q(title__icontains=busqueda)
+      ).distinct()
+      materiales=Material.objects.filter(Q(title__icontains=busqueda)|
+      Q(persona__name__icontains=busqueda)
+      ).distinct()
+   return render(request,"material/list.html",{'temas':temas,'materiales':materiales})
 
-
- 
